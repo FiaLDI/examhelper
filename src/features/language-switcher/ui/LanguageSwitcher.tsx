178d667474d9cf74d4eafa79ac6cd7e61e9e75d3
FiaLDI <leon.youtube2017@yandex.ru>
2026-02-03 21:46:38 +0300

    feat: use themes

--- src/features/language-switcher/ui/LanguageSwitcher.tsx
+++ src/features/language-switcher/ui/LanguageSwitcher.tsx
@@ -6,7 +6,7 @@ export function LanguageSwitcher() {
   const { lang, setLang } = useLanguageStore();
 
   return (
-    <div className="flex gap-2 fixed z-30 right-0 top-0 bg-neutral-800 p-1 text-white text-sm [&>button]:p-2 [&>button]:cursor-pointer [&>button]:data-[current=true]:bg-blue-600 pointer-events-auto">
+    <div className="flex gap-2 fixed z-30 right-0 top-0 bg-background p-1 text-white text-sm [&>button]:p-2 [&>button]:cursor-pointer [&>button]:data-[current=true]:bg-blue-600 pointer-events-auto">
       {allLanguage.map((val, idx) => (
         <button
           key={`lang-btn-${val}-${idx}`}
--- src/features/theme-switcher/ui/ThemeSwitcher.tsx
+++ src/features/theme-switcher/ui/ThemeSwitcher.tsx
@@ -7,7 +7,7 @@ export function ThemeSwitcher() {
   const { theme, setTheme } = useThemeStore();
 
   return (
-    <div className="flex flex-col lg:flex-row gap-2 fixed z-30 left-0 top-0 bg-neutral-800 p-1 text-white text-sm [&>button]:p-2 [&>button]:cursor-pointer [&>button]:data-[current=true]:bg-blue-600 pointer-events-auto">
+    <div className="flex flex-col lg:flex-row gap-2 fixed z-30 left-0 top-0 bg-background p-1 text-white text-sm [&>button]:p-2 [&>button]:cursor-pointer [&>button]:data-[current=true]:bg-blue-600 pointer-events-auto">
       {allTheme.map((val, idx) => (
         <button
             key={`theme-btn-${val}-${idx}`}
